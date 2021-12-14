5f975bf1945ec52bcaf7cf41d84b6768211188b1
Zach Haveraneck <zhaveraneck@oakland.edu>
2021-12-13 21:03:41 -0500

    added package.json

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const ejs = require("ejs");
+const app = new express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+
+app.set("view engine", "ejs");
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get('/lol', function(req, res) {
+  res.render('../views/lol.ejs');
+});
+
+app.get('/Menu', function(req, res) {
+  res.render('../views/Menu.ejs');
+});
+
+app.get('/Order', function(req, res) {
+  res.render('../views/Order.ejs');
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
